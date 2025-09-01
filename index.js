3655d84a7302d8cdb46d3f2bd1548f7d4f2fd193
Qaaed Usaim <mohamedqxxed@gmail.com>
2025-09-02 00:16:53 +0530

    Updated Javascript and CSS

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ let sum = firstCard + secondCard;
 let isAlive = true;
 let hasBlackJack = false;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
-
-//cash out
-console.log(hasBlackJack);
