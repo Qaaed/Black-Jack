1604b1dc355a7d5de76e535ba27b8fc00e42b2be
Qaaed Usaim <mohamedqxxed@gmail.com>
2025-09-02 20:10:29 +0530

    Added new card button

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-//Creating  first card and second card variables
 let firstCard = 4;
 let secondCard = 17;
 let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
 let isAlive = true;
 let hasBlackJack = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  sumEl.textContent = `Sum: ${sum}`;
+  cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
   if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂";
   } else if (sum === 21) {
@@ -19,4 +26,10 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 5;
+  sum += card;
+  renderGame();
 }
